4b8177c3923c2c6cf17c414dbd1fcb27cdfd9a00
Cole Frishman <awesomeeloc@gmail.com>
2020-06-20 12:08:29 -0400

    First release, works

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const interpreter = require('./interpreter');
+const helloworld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+const testprog = ",[-],."
+let out = interpreter.interpret(helloworld, 100, "aa");
+console.log(out);
---
+++ interpreter.js
@@ -0,0 +1,68 @@
+function interpret(sourceCode, memsize, inputText){
+	let input = inputText;
+	const sclength = sourceCode.length;
+	let output = "";
+	
+	let memory = [];
+	let bracketStack = [];
+	let ptr = 0;
+
+
+	for(let i=0; i<memsize; ++i){
+		memory.push(0);
+	}
+	
+	for (let x=0; x<sclength; ++x){
+		var ins = sourceCode.charAt(x);
+		
+		switch (ins) {
+			case '<':
+				--ptr;
+				break;
+			case '>':
+				++ptr;
+				break;
+			case '-':
+				--memory[ptr];
+				break;
+			case '+':
+				++memory[ptr];
+				break;
+			case ',':
+				if(input){
+					memory[ptr] = input.charCodeAt(0);
+					input = input.slice(1);
+				}
+				else{
+
+				}
+				break;
+			case '.':
+				output += String.fromCharCode(memory[ptr]);
+				break;
+			case '[':
+				bracketStack.push(x)
+				break;
+			case ']':
+				if(memory[ptr] === 0){
+					bracketStack.pop()
+				}
+				else{
+					x = bracketStack[bracketStack.length - 1];
+				}
+				break;
+			default:
+				break;
+		}
+
+		if(ptr === -1){
+			ptr = memsize-1;
+		}
+		if(ptr === memsize){
+			ptr = 0;
+		}
+	}
+	return output
+}
+
+module.exports = { interpret }
